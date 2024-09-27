cc7dabefb6161127981d69381d073c2f559339c2
halvor <halvorbmundal@gmail.com>
2024-09-27 15:02:33 +0200

    Endre til å være en handlekurv-app

--- src/main/kotlin/no/digdir/catalog_view_api/config/SecurityConfig.kt
+++ src/main/kotlin/no/digdir/catalog_view_api/config/SecurityConfig.kt
@@ -1,33 +1,20 @@
 package no.digdir.catalog_view_api.config
 
-import com.nimbusds.jose.jwk.RSAKey
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.oauth2.client.endpoint.NimbusJwtClientAuthenticationParametersConverter
-import org.springframework.security.oauth2.client.endpoint.WebClientReactiveAuthorizationCodeTokenResponseClient
-import org.springframework.security.oauth2.client.registration.ClientRegistration
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.JwtIssuerValidator
 import org.springframework.security.oauth2.jwt.JwtTimestampValidator
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
-import java.security.interfaces.RSAPrivateKey
-import java.security.interfaces.RSAPublicKey
 
 @Configuration
-open class SecurityConfig(
-    @Value("classpath:MaskinportenKey.pem")
-    val privateKey: RSAPrivateKey,
-    @Value("classpath:MaskinportenPublicKey.pub")
-    val publicKey: RSAPublicKey,
-) {
+open class SecurityConfig {
     @Bean
     open fun filterChain(
         http: HttpSecurity,
@@ -53,25 +40,5 @@ open class SecurityConfig(
             ),
         )
         return jwtDecoder
-    }
-
-    @Bean
-    open fun webClientReactiveAuthorizationCodeTokenResponseClient(): WebClientReactiveAuthorizationCodeTokenResponseClient {
-        val tokenResponseClient = WebClientReactiveAuthorizationCodeTokenResponseClient()
-
-        tokenResponseClient.addParametersConverter(
-            NimbusJwtClientAuthenticationParametersConverter { clientRegistration: ClientRegistration ->
-                if (clientRegistration.clientAuthenticationMethod.equals(ClientAuthenticationMethod.PRIVATE_KEY_JWT)) {
-                    RSAKey
-                        .Builder(publicKey)
-                        .privateKey(privateKey)
-                        .keyID("Ez5Guxd5I6")
-                        .build()
-                }
-                null
-            },
-        )
-
-        return tokenResponseClient
     }
 }
--- src/main/kotlin/no/digdir/catalog_view_api/controller/AccessApplication.kt
+++ src/main/kotlin/no/digdir/catalog_view_api/controller/AccessApplication.kt
@@ -1,34 +1,92 @@
 package no.digdir.catalog_view_api.controller
 
-import no.digdir.catalog_view_api.client.GetTokenCilent
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 import java.util.UUID
 
 @RestController
 @RequestMapping(value = ["/access-application"], produces = ["application/json"])
-class AccessApplication(
-    val getTokenCilent: GetTokenCilent,
-) {
-    @GetMapping("/{id}")
+class AccessApplication {
+    val webClient =
+        WebClient.create("https://resource.api.fellesdatakatalog.digdir.no")
+
+    @GetMapping("/{type}/{id}")
     fun test(
         @PathVariable id: UUID,
-    ): ResponseEntity<String> {
-        // Få maskinporten token med scope altinn:serviceowner/instances.write
+        @PathVariable type: String,
+    ): ResponseEntity<Handlekurv> {
+        val test =
+            webClient
+                .get()
+                .uri("/$type/$id")
+                .retrieve()
+                .bodyToMono(Metadata::class.java)
+                .block()
 
-        // Byttes inn til Altinn token
-
-        // opprette en ny instans i altinn. POST {basePath}/create
-        // Base path: {org}/{appname}/instances
-        // digdir/tilgangssoknad/instances
-
-        // På altinnsiden må vi opprette en instans som peker
-
-        getTokenCilent.test()
-
-        return ResponseEntity.ok(id.toString())
+        return ResponseEntity.ok(test.toHandlekurv(id))
     }
+}
+
+enum class AccessRight {
+    PUBLIC,
+    RESTRICTED,
+    NON_PUBLIC,
+}
+
+data class Metadata(
+    val contactPoint: List<ContactPoint>,
+    val title: Title,
+    val publisher: Publisher,
+    val admsIdentifier: String?,
+    val accessRights: AccessRights?,
+) {
+    data class ContactPoint(
+        val email: String,
+    )
+
+    data class Title(
+        val nb: String?,
+        val en: String?,
+    )
+
+    data class Publisher(
+        // samme som orgnr
+        val id: String,
+    )
+
+    data class AccessRights(
+        val code: AccessRight,
+    )
+
+    fun toHandlekurv(uuid: UUID): Handlekurv =
+        Handlekurv(
+            orgnr = publisher.id,
+            dataDef =
+                Handlekurv.DataDef(
+                    eksternId = admsIdentifier,
+                    katalogId = uuid.toString(),
+                    orgnr = publisher.id,
+                    navn = title.nb ?: title.en ?: "",
+                ),
+            hintIsPublic = accessRights?.code == AccessRight.PUBLIC,
+        )
+}
+
+data class Handlekurv(
+    val orgnr: String,
+    val dataDef: DataDef,
+    val hintIsPublic: Boolean,
+) {
+    val system: String = "datanorge"
+
+    data class DataDef(
+        val eksternId: String?,
+        val katalogId: String,
+        val orgnr: String,
+        val navn: String,
+    )
 }
